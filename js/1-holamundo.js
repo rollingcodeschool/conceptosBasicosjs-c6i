a654b7af1d3ed2458957827558ab775a73693419
Emilse Arias <emilsearias@gmail.com>
2022-06-28 18:03:21 -0300

    agreamos mensajes por diferentes pantallas

---
+++ js/1-holamundo.js
@@ -0,0 +1,19 @@
+//Esto es un comentario de una unica linea
+
+/* Esto es un comentario
+de multiples
+lineas */
+
+// mostrar un mensaje en la pantalla o documento
+document.write('<h1>Hola comision 6i</h1>');
+
+//mostrar un mensaje por consola
+console.log("prueba de consola");
+
+//mostrar un mensaje en una ventana emergente
+alert('desde una ventana emergente');
+
+
+
+
+
