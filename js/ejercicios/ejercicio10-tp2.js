042548e103c32127658489c86c1bd6f9b6983a21
Emilse Arias <emilsearias@gmail.com>
2022-07-12 18:35:11 -0300

    agregamos el valor a las celdas del ejercicio

--- js/ejercicios/ejercicio10-tp2.js
+++ js/ejercicios/ejercicio10-tp2.js
@@ -4,20 +4,21 @@
 
 //cada celda debe tener un numero descendente de multiplicar filas por columnas
 
-let filas = parseInt(prompt('Ingrese un numero de filas'));
-let columnas = parseInt(prompt('Ingrese un numero de columnas'));
+const filas = parseInt(prompt('Ingrese un numero de filas'));
+const columnas = parseInt(prompt('Ingrese un numero de columnas'));
+let resultado = filas * columnas;
 
 document.write(`<table> <tbody>`)
 
 //este se encarga de dibujar las fijas
 for(let indiceFila = 0; indiceFila < filas; indiceFila++){
-    document.write(`
-        <tr>
-            <td>prueba</td>
-            <td>datos</td>
-        </tr>`)
+    document.write(`<tr>`)
+    for(let indiceColumna=0; indiceColumna< columnas; indiceColumna++ ){
+        document.write(`<td>${resultado}</td>`); // document.write('<td>'+ resultado +'</td>')
+        resultado--;
+    }
+    document.write(`</tr>`)
 }
-
 
 document.write(`</tbody> </table>`)
 
