c0e792ff285fe679952df23f008f644be49d28c9
Emilse Arias <emilsearias@gmail.com>
2022-07-19 17:47:28 -0300

    actualizamos el codigo del archivo de arreglos con funciones

--- js/5-arreglos.js
+++ js/5-arreglos.js
@@ -2,60 +2,42 @@
 let listaSupermercado = [];
 
 //crear un arreglo con datos
-let series = ['peaky blinders', 7 , 'stranger things',4, 'the boys', 3, 'loky'];
+let series = ["peaky blinders", 7, "stranger things", 4, "the boys", 3, "loky"];
+
+let mostrarSeries = (titulo) => {
+  document.write("<h3>"+titulo+"</h3>" );
+  document.write(`<ul>`);
+  for (let indice = 0; indice < series.length; indice++) {
+    document.write(`<li>${series[indice]}</li>`);
+  }
+  document.write(`</ul>`);
+};
 
 //mostrar un arreglo
 document.write(series);
 //mostrar un elemento del arreglo
-document.write('<br>'+series[0]);
+document.write("<br>" + series[0]);
 
 //mostrar un arreglo dentro de una lista
 //series.length devuelve la cantidad de elementos en el arreglo
-document.write(`<ul>`);
-for(let indice=0; indice < series.length; indice++ ){
-    document.write(`<li>${series[indice]}</li>`)
-}
-document.write(`</ul>`);
+mostrarSeries('Arreglo original');
 
 //agregar un elemento nuevo al arreglo al final
-series.push('Rick and Morty');
-
-document.write('<h3>Agregamos un nuevo elemento al final del arreglo</h3>')
-document.write(`<ul>`);
-for(let indice=0; indice < series.length; indice++ ){
-    document.write(`<li>${series[indice]}</li>`)
-}
-document.write(`</ul>`);
+series.push("Rick and Morty");
+mostrarSeries('Agregamos un nuevo elemento al final del arreglo');
 
 //agregar un elemento en una posicion particular del arreglo
-series.splice(2,0,'The witcher'); // (posicion del elemento, cantidad de elementos a borrar, item que vamos agregar)
-
-document.write('<h3>Agregamos un nuevo elemento en una posicion particular</h3>')
-document.write(`<ul>`);
-for(let indice=0; indice < series.length; indice++ ){
-    document.write(`<li>${series[indice]}</li>`)
-}
-document.write(`</ul>`);
+series.splice(2, 0, "The witcher"); // (posicion del elemento, cantidad de elementos a borrar, item que vamos agregar)
+mostrarSeries('Agregamos un nuevo elemento en una posicion particular');
 
 //eliminar elementos
-series.splice(4,1); //(posicion del elemento, cantidad de elementos a borrar)
-
-document.write('<h3>Eliminar un elemento de una posicion particular</h3>')
-document.write(`<ul>`);
-for(let indice=0; indice < series.length; indice++ ){
-    document.write(`<li>${series[indice]}</li>`)
-}
-document.write(`</ul>`);
+series.splice(4, 1); //(posicion del elemento, cantidad de elementos a borrar)
+mostrarSeries('Eliminar un elemento de una posicion particular');
 
 //eliminar varios elementos
 // series.splice(2); borrar todos los elementos desde la posicion 2
 
 //modificar elementos en un arreglo
-series[1] = 'Suits';
+series[1] = "Suits";
+mostrarSeries('Modificar un elemento de una posicion particular');
 
-document.write('<h3>Modificar un elemento de una posicion particular</h3>')
-document.write(`<ul>`);
-for(let indice=0; indice < series.length; indice++ ){
-    document.write(`<li>${series[indice]}</li>`)
-}
-document.write(`</ul>`);
