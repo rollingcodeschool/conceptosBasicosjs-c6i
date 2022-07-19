107743fc26848d47b522210d1ca6f2581332d215
Emilse Arias <emilsearias@gmail.com>
2022-07-19 18:38:21 -0300

    agregamos bucles para trabajar el ejercicio de los dados

---
+++ js/ejercicios/ejercicio3-tp3.js
@@ -0,0 +1,38 @@
+// simule el lanzamiento de dos dados  Math.random
+//Sumar el resultado de lanzar dos dados y anotar en un array
+//mostrar con que frecuencia aparecen los resultados de todas las sumas
+
+// let dado1 = Math.floor(Math.random()* (max - min +1)+min);
+
+let sumas = [];
+let pruebaDado1 = [];
+let pruebaDado2 = [];
+
+for (let i = 0; i < 10; i++) {
+  let dado1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  let dado2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  let suma = dado1 + dado2;
+  sumas.push(suma);
+  pruebaDado1.push(dado1);
+  pruebaDado2.push(dado2);
+}
+
+document.write('Resultados del primer dado: '+ pruebaDado1);
+document.write('<br>Resultados del segundo dado: '+pruebaDado2);
+document.write('<br>Resultados de la suma: '+sumas);
+
+
+document.write(`<table> 
+<tbody>
+<tr><td>Resultado</td><td>Frecuencia</td></tr>
+`)
+for( let resultado =2; resultado <=12; resultado++){
+    for(let posicion =0; posicion < sumas.length; posicion++){
+        if(resultado === sumas[posicion]){
+            //sumar las apariciones
+        }
+    }
+    document.write(`<tr><td>${resultado}</td><td>x</td></tr>`);
+}
+
+document.write(`</tbody></table>`)
